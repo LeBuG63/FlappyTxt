331c7cd08abe0f8e5f961abaf7677bebbc16834a
Le_BuG63 <lebug63@gmail.com>
2014-04-10 18:26:54 +0200

    Firt commit

---
+++ cmd.c
@@ -0,0 +1,34 @@
+#include "cmd.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_CMD_LENGHT	12
+
+int
+get_command(void) {
+
+	char	cmd[MAX_CMD_LENGHT] = {NULL};
+
+	fgets(cmd, MAX_CMD_LENGHT, stdin);
+
+	for(int i = 0; cmd[i] != '\0'; ++i)
+		cmd[i] = toupper(cmd[i]);
+
+	if(*cmd != '\n') {
+		if(memcmp(cmd, "UP", 2) == 0) {
+			return (int)(cmd[3] - '0');
+		}
+		if(memcmp(cmd, "DOWN", 4) == 0) {
+			return -(int)(cmd[5] - '0');
+		}
+		if(memcmp(cmd, "HELP", 4) == 0) {
+			return CMDHELP;
+		}
+		if(memcmp(cmd, "EXIT", 4) == 0) {
+			return CMDEXIT;
+		}
+	}
+
+	return -1;
+}
